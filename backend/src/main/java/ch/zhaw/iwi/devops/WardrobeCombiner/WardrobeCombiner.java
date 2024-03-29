14ddd7d09ccdf6f0abf052a821b818b74f851090
Matthias Galanti <matthias.galanti@halter.ch>
2024-03-29 16:13:14 +0100

    TestComplete

---
+++ backend/src/main/java/ch/zhaw/iwi/devops/WardrobeCombiner/WardrobeCombiner.java
@@ -0,0 +1,16 @@
+package ch.zhaw.iwi.devops.WardrobeCombiner;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class WardrobeCombiner {
+
+    private int targetSize;
+
+    public WardrobeCombiner(int targetSize) {
+        this.targetSize = targetSize;
+    }
+
+    
+}
