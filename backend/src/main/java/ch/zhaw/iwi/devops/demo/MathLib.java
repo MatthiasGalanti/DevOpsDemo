ebc690e9da9cabde07efbc7717a179ebc713ead3
Matthias Galanti <matthias.galanti@halter.ch>
2024-04-29 10:47:37 +0200

    Week 10 Change_1

--- backend/src/main/java/ch/zhaw/iwi/devops/demo/MathLib.java
+++ backend/src/main/java/ch/zhaw/iwi/devops/demo/MathLib.java
@@ -5,5 +5,10 @@ public class MathLib {
     public static boolean isEven(int value) {
         return value % 2 == 0;
     }
+
+    // Week 10 Change 
+    public static boolean isOdd(int value) {
+        return value % 2 != 0;
+    }
     
 }
